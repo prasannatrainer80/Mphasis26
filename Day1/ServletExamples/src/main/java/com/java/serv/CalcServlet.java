f2a23c1ab414c56eb4492a772f17c686e6e698dc
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-01-13 16:35:39 +0530

    code

---
+++ Day1/ServletExamples/src/main/java/com/java/serv/CalcServlet.java
@@ -0,0 +1,50 @@
+package com.java.serv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalcServlet
+ */
+public class CalcServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		int firstNo, secondNo, result;
+		firstNo = Integer.parseInt(request.getParameter("firstNo"));
+		secondNo = Integer.parseInt(request.getParameter("secondNo"));
+		result = firstNo + secondNo;
+		out.println("Sum is <b> " +result+ "<br/>");
+		result = firstNo - secondNo;
+		out.println("Sub is <b> " +result+ "<br/>");
+		result = firstNo * secondNo;
+		out.println("Mult is <b>  " +result+ "<br/>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
