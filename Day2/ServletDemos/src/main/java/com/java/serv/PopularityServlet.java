5e835994804b7848816a5dcd8e424e164502efde
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-01-14 14:30:49 +0530

    code

---
+++ Day2/ServletDemos/src/main/java/com/java/serv/PopularityServlet.java
@@ -0,0 +1,52 @@
+package com.java.serv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PopularityServlet
+ */
+public class PopularityServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	static int count;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PopularityServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		count = 0;
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		count++;
+		PrintWriter out = response.getWriter();
+		out.println("No.Of Users Accessed My Website are <b> " +count+ "</b><br/>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Day2/ServletDemos/src/main/java/com/java/serv/ShowParameters.java
@@ -0,0 +1,53 @@
+package com.java.serv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowParameters
+ */
+public class ShowParameters extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private String company;
+	private String trainer;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShowParameters() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		company = config.getInitParameter("company");
+		trainer = config.getInitParameter("trainer");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("Company Name is <b> " +company+ "</b><br/>");
+		out.println("Trainer Name is <b> " +trainer+ "</b><br/>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
