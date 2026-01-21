7f615da614c99138cf074943399aa2d354ac9b39
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-01-21 11:43:02 +0530

    code

---
+++ Day8/Login.sql
@@ -0,0 +1,11 @@
+use mphasis26;
+
+create table Login
+(
+   userName varchar(30) Primary Key,
+   passCode varchar(30) NOT NULL
+);
+
+Insert into Login values('Shiva','Pallavi'),
+('Vamsi','Krishna'),('Sridhar','Reddy'),
+('Mythri','Mythri')
