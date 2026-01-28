0775d74fa47c17509add7225c546dc17beebb9b5
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-01-28 14:55:38 +0530

    code

--- Day13/jdbc/EmployService.java
+++ Day13/jdbc/EmployService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -20,6 +21,13 @@ public class EmployService {
 		context = 
 		new ClassPathXmlApplicationContext("com/java/rest/jdbc.xml");
 		employDao = (EmployDaoImpl)context.getBean("employDaoImpl");
+	}
+	
+	@GET
+	@Path("/searchemploy/{empno}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Employ searchEmploy(@PathParam("empno") int empno) {
+		return employDao.searchEmploy(empno);
 	}
 	
 	@GET
