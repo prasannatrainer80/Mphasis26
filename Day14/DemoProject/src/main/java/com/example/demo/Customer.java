460a45d351b4a27e54118031645b37a4387f7c81
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-01-29 14:56:46 +0530

    code

---
+++ Day14/DemoProject/src/main/java/com/example/demo/Customer.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Customer {
+	
+	private int custId;
+	private String customerName;
+	private String city;
+	private String mobileNo;
+	private double billAmount;
+	
+	
+}
---
+++ Day14/DemoProject/src/main/java/com/example/demo/EmployController.java
@@ -0,0 +1,45 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployController {
+
+	@Autowired
+	private EmployDao employDao;
+	
+	@DeleteMapping(value="/deleteemploy/{empno}")
+	public String deleteEmploy(@PathVariable int empno) {
+		return employDao.deleteEmploy(empno);
+	}
+	
+	@PutMapping(value="/updateemploy")
+	public String updateEmploy(@RequestBody Employ employ) {
+		return employDao.updateEmploy(employ);
+	}
+	
+	@PostMapping(value="/addemploy")
+	public String addEmploy(@RequestBody Employ employ) {
+		return employDao.addEmploy(employ);
+	}
+	
+	@GetMapping(value="/searchemploy/{empno}")
+	public Employ searchEmploy(@PathVariable int empno) {
+		return employDao.searchEmploy(empno);
+	}
+	
+	@GetMapping(value="/showemploy")
+	public List<Employ> showEmploy() {
+		return employDao.showEmploy();
+	}
+}
