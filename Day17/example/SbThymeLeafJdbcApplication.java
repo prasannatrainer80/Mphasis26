7b20f7a257f9f8f5708dbcd5520dcdcca38f53c1
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-02-02 14:11:47 +0530

    code

---
+++ Day17/example/SbThymeLeafJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbThymeLeafJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbThymeLeafJdbcApplication.class, args);
+	}
+
+}
