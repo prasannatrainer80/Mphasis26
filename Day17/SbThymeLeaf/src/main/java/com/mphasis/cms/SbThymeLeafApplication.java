43739d52c636b756ff4433892a22339a4b281230
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2026-02-02 15:25:23 +0530

    code

---
+++ Day17/SbThymeLeaf/src/main/java/com/mphasis/cms/SbThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.mphasis.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbThymeLeafApplication.class, args);
+	}
+
+}
